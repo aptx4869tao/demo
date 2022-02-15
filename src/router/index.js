1605187850542956415f3e2f44523d39024a31ad
aptx4869tao <t2062558563@gmail.com>
2022-02-15 09:09:38 +0800

    add

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,16 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Index from '../views/index.vue'
+// import Index from '../views/index.vue'
 import Nav from '../views/nav.vue'
+import Set from '../views/$set.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'index',
-    component: Index
+    name: 'Set',
+    component: Set
   }, {
     path: '/nav',
     name: 'nav',
---
+++ src/views/$set.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <div v-for="(item,index) in list" :key='index'>
+        {{item.name}}{{item.id}}
+    </div>
+    <button @click="changeData">改变</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Set',
+  data () {
+    return {
+      list: [
+        { name: '小明', id: 16 },
+        { name: '小张', id: 18 }
+      ]
+    }
+  },
+  mounted () {
+    // this.list[0] = { name: '张三', id: 20 }
+    console.log(this.list)
+  },
+  methods: {
+    changeData () {
+      this.list[0] = { name: '张三', id: 20 }
+      //   this.$set(this.list, 2, { name: '李四', id: 22 }) // 改变的数据源 要改变\添加具体数据 重新赋的值
+      console.log(this.list)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
